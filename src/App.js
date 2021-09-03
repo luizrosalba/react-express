09111b88e5e39ca462ed86ed3da52f3b79c173e9
Luiz Rosalba <luiz.rosalba@deepseedsolutions.com>
2021-09-03 10:13:04 -0300

    REXP: Adding A server Alive render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
+
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+
+import * as req from "./api/AppRequests";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [alive, setAlive] = useState(false);
+
+  useEffect(() => {
+      let temp = req.serverAlive();
+      temp.then(
+        result => {
+          setAlive(result)
+        }
+      )
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {alive?
+          <div>Server Alive</div>
+          :
+          <div>Server not Alive </div>
+        }
+    
       </header>
+      
     </div>
   );
 }
